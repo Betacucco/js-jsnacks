9a88739797cf0ae156dbb8d62af263efb1866490
MarcoPieretti <marcopieretti50@gmail.com>
2022-03-25 16:14:49 +0100

    ottavo js-snack

---
+++ js-snack8/main.js
@@ -0,0 +1,21 @@
+
+let arrayDiNumeri = [];
+let numeroDaInserire;
+
+for(let i = 0; i < 10; i++){
+    numeroDaInserire = parseInt(prompt("Inserisci numero nell'array"));
+    arrayDiNumeri.push(numeroDaInserire);
+}
+console.log(arrayDiNumeri);
+
+let risultato = sommmaNumeri(arrayDiNumeri);
+
+
+function sommmaNumeri(arrayDiNumeri){
+    let somma = 0;
+    
+    for(let i = 0; i < 10; i++){
+        somma += arrayDiNumeri[i]; 
+    }
+    console.log("La somma dei numeri nel mio array e': " + somma);
+}
