fa829ad245bfa3a5538882949527d7dfb3764343
MarcoPieretti <marcopieretti50@gmail.com>
2022-03-25 14:02:25 +0100

    Terzo j-snack

---
+++ js-snack3/main.js
@@ -0,0 +1,9 @@
+let somma = 0;
+
+for(let i = 0; i < 10; i++){
+    let numero = parseInt(prompt("Inserisci numero: "));
+    console.log(numero);
+    somma = somma + numero;
+}
+
+console.log(somma);
