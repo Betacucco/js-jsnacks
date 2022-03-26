2f0e56653992632511e03699f6f06cd9eebcb65b
MarcoPieretti <marcopieretti50@gmail.com>
2022-03-26 17:27:36 +0100

    Corretto esercizio 8

--- js-snack8/main-snack8.js
+++ js-snack8/main-snack8.js
@@ -1,23 +1,15 @@
 //Dichiariamo il nostro array vuoto e la variabile dei numeri da inserire
-let arrayDiNumeri = [];
-let numeroDaInserire;
+let arrayDiNumeri = [5, 48, 23, 8, 4, 7, 6, 72, 9, 1];
 
-//Riempiamo il nostro array
-for(let i = 0; i < 10; i++){
-    numeroDaInserire = parseInt(prompt("Inserisci numero nell'array"));
-    arrayDiNumeri.push(numeroDaInserire);
-}
-console.log(arrayDiNumeri);
-
-//Richiamiamo la funzione con la somma
-let risultato = sommmaNumeri(arrayDiNumeri);
+//Chiamiamo la funzione che fa la somma e stampa il risultato
+sommmaNumeri();
 
 //Funzione della somma
-function sommmaNumeri(arrayDiNumeri){
+function sommmaNumeri(){
     let somma = 0;
     
     //Nel nostro for usiamo la variabile somma per mantenerci la somma di tutti gli elementi dell'array che scorriamo
-    for(let i = 0; i < 10; i++){
+    for(let i = 0; i < arrayDiNumeri.length; i++){
         somma += arrayDiNumeri[i]; 
     }
     console.log("La somma dei numeri nel mio array e': " + somma);
