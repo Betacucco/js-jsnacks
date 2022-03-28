b2f01f5ac675e80214ff25b3414dbaa5b29c9a8d
MarcoPieretti <marcopieretti50@gmail.com>
2022-03-28 10:18:16 +0200

    Piccole correzioni

--- js-snack7/main.js
+++ js-snack7/main.js
@@ -9,7 +9,7 @@ console.log("La somma dei numeri al cubo e': " + risultato);
 function stampaCuboNumeriDa0aN(N){
     let somma = 0;
 
-    for(let i = 0; i < N; i++){
+    for(let i = 1; i <= N; i++){
         let cubo =  Math.pow(i, 3);
         console.log("Il numero al cubo e': " + cubo);
 
--- js-snack9/main-snack9.js
+++ js-snack9/main-snack9.js
@@ -1,28 +1,32 @@
 //Dichiariamo il nostro array vuoto e la variabile dei numeri da inserire
 let arrayDiNumeri = [];
-let numeroDaInserire;
 
 //Riempiamo il nostro array
 for(let i = 0; i < 10; i++){
-    numeroDaInserire = parseInt(prompt("Inserisci numero nell'array"));
+    let numeroDaInserire = parseInt(prompt("Inserisci numero nell'array"));
     arrayDiNumeri.push(numeroDaInserire);
 }
 console.log(arrayDiNumeri);
 
 //Richiamiamo la funzione con la media
-let risultato = mediaAritmeticaNumeri(arrayDiNumeri);
+let mediaFinale = mediaAritmeticaNumeri(arrayDiNumeri);
+console.log("La media dei numeri nel mio array e': " + mediaFinale);
 
-//Funzione della media
-function mediaAritmeticaNumeri(arrayDiNumeri){
-let somma = 0;
-let media = 0;
-
-//Nel nostro for usiamo la variabile somma per mantenerci la somma di tutti gli elementi dell'array che scorriamo
-for(let i = 0; i < 10; i++){
-    somma += arrayDiNumeri[i]; 
+//Funzione della somma
+function sommaNumeri(array){
+    let somma = 0;
+    
+    //Nel nostro for usiamo la variabile somma per mantenerci la somma di tutti gli elementi dell'array che scorriamo
+    for(let i = 0; i < 10; i++){
+        somma += array[i]; 
+    }
+    return somma;
 }
-    //Media potrebbe essere con la virgola e la fisso con un massimo di 3 decimali
-    media = parseFloat(somma / 10);
-    media.toFixed(3);
-    console.log("La media dei numeri nel mio array e': " + media);
+//Funzione della media
+function mediaAritmeticaNumeri(inputArray){
+
+    let somma = sommaNumeri(inputArray);
+    //Media
+    let media = parseFloat(somma / 10);
+    return media;
 }
